04479b089bb39aa82882f50f195e5a36490b63ee
Amiralgaby <gaby.theuws@gmail.com>
2022-03-08 21:22:29 +0100

    suppression de chose inutiles

--- create_file.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file = 'vul_log.txt';
-// The new person to add to the file
-$person = "Linus Torvalds\n";
-// Write the contents to the file,
-// using the FILE_APPEND flag to append the content to the end of the file
-// and the LOCK_EX flag to prevent anyone else writing to the file at the same time
-file_put_contents($file, $person, FILE_APPEND | LOCK_EX);
-?>
